d307deea64a3ad52ebc16b6d745158c623e8e3b5
LTprograms <105554066+LTprograms@users.noreply.github.com>
2025-07-02 06:03:17 -0500

    semaforo funciona v2

--- js/agent.js
+++ js/agent.js
@@ -40,4 +40,76 @@ class Agent {
         }
         return betterAction;
     }
+
+    updateTrafficStateFromDOM() {
+  
+        const updateAxis = (axis, type) => {
+            const isPerson = type === "person";
+            const className = isPerson ? `.person${axis}` : `.carro${axis}`;
+            const elements = document.querySelectorAll(className);
+            const {side, client} = TRANSLATOR[axis];
+            const config = isPerson ? CONFIG.person : CONFIG.car
+
+            let minDistance = Infinity;
+                elements.forEach(el => {
+                const parentSize = el.parentElement[client];
+                const pos = parseFloat(getComputedStyle(el)[side]);
+                const ratio = pos / parentSize;
+
+                const isBeforeCross =  ratio < config.intervals.max
+                if (isBeforeCross) {
+                    if (isPerson) {
+                        agent.counts[`p${axis.at(-1).toLowerCase()}`]++; // pedestrians
+                        if (el.src.includes("sp")) { // special pedestrians
+                            agent.counts[`sp${axis.at(-1).toLowerCase()}`]++;
+                        }
+                    } else {
+                        agent.counts[`n${axis.at(-1).toLowerCase()}`]++; // cars
+                        if (el.src.includes("sc")) {
+                            agent.counts[`sc${axis.at(-1).toLowerCase()}`]++
+                        };
+                    }
+                    if (ratio < minDistance) {
+                        minDistance = ratio;
+                    }
+                } else {
+                    if (el.dataset.passed === "false") {
+                        el.dataset.passed = "true";
+                        if (isPerson) {
+                            agent.counts[`p${axis.at(-1).toLowerCase()}`]--;
+                            if (el.src.includes("sp")) {
+                                agent.counts[`sp${axis.at(-1).toLowerCase()}`]--
+                            };
+                        } else {
+                            agent.counts[`n${axis.at(-1).toLowerCase()}`]--;
+                            if (el.src.includes("sc")) {
+                                agent.counts[`sc${axis.at(-1).toLowerCase()}`]--
+                            };
+                        }
+                    }
+                }
+            });
+            if (!isPerson) {
+                const distProp = axis.at(-1).toLowerCase() === "y" ? "dy" : "dx";
+                agent.counts[distProp] = minDistance; // normalizamos distancia
+                // console.log(agent.counts[distProp])
+            }
+        };
+
+        ["Y", "-Y", "X", "-X"].forEach(axis => {
+            updateAxis(axis, "car");
+            updateAxis(axis, "person");
+        });
+        // Asignar al estado principal
+        agent.state.ny = normalizeNumberCars(agent.counts.ny);
+        agent.state.nx = normalizeNumberCars(agent.counts.nx);
+        agent.state.py = normalizeNumberCars(agent.counts.py);
+        agent.state.px = normalizeNumberCars(agent.counts.px);
+        agent.state.spy = agent.counts.spy > 0 ? 1 : 0;
+        agent.state.spx = agent.counts.spx > 0 ? 1 : 0;
+        agent.state.scy = agent.counts.scy > 0 ? 1 : 0;
+        agent.state.scx = agent.counts.scx > 0 ? 1 : 0;
+        agent.state.dy = normalizeDistance(agent.counts.dy);
+        agent.state.dx = normalizeDistance(agent.counts.dx);
+    }
 }
--- js/script.js
+++ js/script.js
@@ -1,76 +1,10 @@
-
-
 const agent = new Agent();
-
-
-function updateTrafficStateFromDOM() {
-  
-  const updateAxis = (axis, type) => {
-    const isPerson = type === "person";
-    const className = isPerson ? `.person${axis}` : `.carro${axis}`;
-    const elements = document.querySelectorAll(className);
-    const {side, client} = TRANSLATOR[axis];
-
-    let minDistance = Infinity;
-    elements.forEach(el => {
-      const parentSize = el.parentElement[client];
-      const pos = parseFloat(getComputedStyle(el)[side]);
-      const ratio = pos / parentSize;
-
-      const isBeforeCross =
-        (axis === "Y" || axis === "-Y") ? (ratio <= 0.34) : (ratio <= 0.34);
-      if (isBeforeCross) {
-        if (isPerson) {
-          agent.counts[`p${axis.at(-1).toLowerCase()}`]++;
-          if (el.src.includes("sp")) agent.counts[`sp${axis.at(-1).toLowerCase()}`]++;
-        } else {
-          agent.counts[`n${axis.at(-1).toLowerCase()}`]++;
-          if (el.src.includes("sc")) agent.counts[`sc${axis.at(-1).toLowerCase()}`]++;
-        }
-        if (ratio < minDistance) {
-          minDistance = ratio;
-        }
-      } else {
-        if (el.dataset.passed === "false") {
-          el.dataset.passed = "true";
-          if (isPerson) {
-            agent.counts[`p${axis.at(-1).toLowerCase()}`]--;
-            if (el.src.includes("sp")) agent.counts[`sp${axis.at(-1).toLowerCase()}`]--;
-          } else {
-            agent.counts[`n${axis.at(-1).toLowerCase()}`]--;
-            if (el.src.includes("sc")) agent.counts[`sc${axis.at(-1).toLowerCase()}`]--;
-          }
-        }
-      }
-    });
-    if (!isPerson) {
-      const distProp = axis.at(-1).toLowerCase() === "y" ? "dy" : "dx";
-    //   agent.counts[distProp] = 1 - minDistance; // normalizamos distancia
-    }
-  };
-
-  ["Y", "-Y", "X", "-X"].forEach(axis => {
-    updateAxis(axis, "car");
-    updateAxis(axis, "person");
-  });
-  // Asignar al estado principal
-  agent.state.ny = normalizeNumberCars(agent.counts.ny);
-  agent.state.nx = normalizeNumberCars(agent.counts.nx);
-  agent.state.py = normalizeNumberCars(agent.counts.py);
-  agent.state.px = normalizeNumberCars(agent.counts.px);
-  agent.state.spy = agent.counts.spy > 0 ? 1 : 0;
-  agent.state.spx = agent.counts.spx > 0 ? 1 : 0;
-  agent.state.scy = agent.counts.scy > 0 ? 1 : 0;
-  agent.state.scx = agent.counts.scx > 0 ? 1 : 0;
-  agent.state.dy = agent.counts.dy;
-  agent.state.dx = agent.counts.dx;
-}
 
 setInterval(() => {
   if (!trafficEnabledX) agent.state.tw++;
   if (!trafficEnabledY) agent.state.tw++;
   if (isAmbar) return;
-  updateTrafficStateFromDOM();
+  agent.updateTrafficStateFromDOM();
   
   const { nx, ny, al, tw, py, px, spy, spx, scy, scx, dy, dx } = agent.state;
   const stateString = `${ny}_${nx}_${al}_${normalizeTime(tw)}_${py}_${px}_${spy}_${spx}_${scy}_${scx}_${dy}_${dx}`;
